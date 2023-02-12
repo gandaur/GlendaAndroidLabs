3a4b98f15776b03ab31a0db8a07b05bd9e3915f9
gandaur <114960294+gandaur@users.noreply.github.com>
2023-02-12 18:22:14 -0500

    Lab 4 part 2

--- app/src/main/java/algonquin/cst2335/anda0017/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/anda0017/MainActivity.java
@@ -1,41 +1,86 @@
 package algonquin.cst2335.anda0017;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import algonquin.cst2335.anda0017.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    private static String TAG = "MainActivity";
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+        getSupportActionBar().hide();
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SharedPreferences preferences= getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        Button loginButton = findViewById(R.id.login);
+        EditText text = findViewById(R.id.editTextPassword);
+        EditText emailtext= findViewById(R.id.emailText);
+
+        binding.login.setOnClickListener(btn -> {
+
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            Intent EmailAddress  = nextPage.putExtra("EmailAddress", emailtext.getText().toString());
+                nextPage.putExtra("Age", 34);
+                nextPage.putExtra("Name","Glenda");
+                nextPage.putExtra("PostalCode", "J8X 3R9");
+
+                //makes the transition, send the data to SecondActivity
+                startActivity(nextPage);
+            });
+
+
     }
-}
+    @Override // Activity is now visible
+    protected void onStart() {
+
+        super.onStart();
+        Log.w(TAG, "The application is now visible");
+    }
+
+    @Override //listens for touch
+    protected void onResume() {
+
+        super.onResume();
+        Log.w(TAG, "The application now listens for touch");
+    }
+
+    @Override //no longer listening to touches
+    protected void onPause() {
+
+        super.onPause();
+        Log.w(TAG, "The application no longer listens to touches");
+    }
+
+
+    @Override // no longer visible
+    protected void onStop() {
+
+        super.onStop();
+        Log.w(TAG, "The application is no longer visible");
+    }
+
+    @Override // opposite of onCreate, memory is garbage collected
+    protected void onDestroy() {
+
+        super.onDestroy();
+        Log.w(TAG, "Memory used by the application is garbaged");
+    }
+
+
+    }
+
---
+++ app/src/main/java/algonquin/cst2335/anda0017/SecondActivity.java
@@ -0,0 +1,110 @@
+package algonquin.cst2335.anda0017;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        //returns an intent object
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
+        TextView email = findViewById(R.id.textView3);
+        email.setText("Welcome Back " + emailAddress);
+
+        Button phoneCall = (Button) findViewById(R.id.call);
+        EditText phoneNumber = (EditText) findViewById(R.id.editTextNumber);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPhoneData", Context.MODE_PRIVATE);
+        String phone = sharedPreferences.getString("Phone number", "");
+        phoneNumber.setText(phone);
+
+        phoneCall.setOnClickListener(clk -> {
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel: " + phoneNumber.getText()));
+            startActivity(call);
+        });
+
+        ImageView profileImage = (ImageView) findViewById(R.id.imageButton);
+        Button changePicture = (Button) findViewById(R.id.button3);
+
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap thumbnail = data.getParcelableExtra("data");
+
+
+                        FileOutputStream fOut = null;
+
+                        File file = new File( getFilesDir(), "Picture.png");
+
+                        if(file.exists())
+
+                        {
+                            Bitmap imageButton = BitmapFactory.decodeFile("/data/data/algonquin.cst2335.anda0017/files/Picture.png");
+                            profileImage.setImageBitmap( thumbnail);
+                        }
+                        try {
+                            fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                            thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        changePicture.setOnClickListener(clk -> {
+            cameraResult.launch(cameraIntent);
+        });
+    }
+
+    protected void onPause() {
+        super.onPause();
+
+        EditText phoneNumber = (EditText) findViewById(R.id.editTextNumber);
+
+        SharedPreferences sharedPreferences = getSharedPreferences
+                ("MyPhoneData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putString("Phone number" , phoneNumber.getText().toString());
+        ed.apply();
+    }
+}
+
+
+
+
+
+
+
+
