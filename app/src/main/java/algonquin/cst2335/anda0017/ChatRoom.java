7e48629b8b4586c74281874cd92825ca067e5590
gandaur <114960294+gandaur@users.noreply.github.com>
2023-03-03 05:38:56 -0500

    Finished Recycler view

---
+++ app/src/main/java/algonquin/cst2335/anda0017/ChatRoom.java
@@ -0,0 +1,115 @@
+package algonquin.cst2335.anda0017;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import algonquin.cst2335.anda0017.data.ChatMessage;
+import algonquin.cst2335.anda0017.data.ChatRoomViewModel;
+import algonquin.cst2335.anda0017.databinding.ActivityChatRoomBinding;
+import algonquin.cst2335.anda0017.databinding.ReceiveMessageBinding;
+import algonquin.cst2335.anda0017.databinding.SentMessageBinding;
+
+
+public class ChatRoom extends AppCompatActivity {
+
+    private ActivityChatRoomBinding binding;
+    private RecyclerView.Adapter<MyRowHolder> myAdapter;
+    ArrayList<ChatMessage> messages;
+    ChatRoomViewModel chatModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityChatRoomBinding.inflate((getLayoutInflater()));
+        setContentView(binding.getRoot());
+
+        chatModel = new ViewModelProvider(this).get(ChatRoomViewModel.class);
+        messages = chatModel.messages.getValue();
+        if (messages == null) {
+            chatModel.messages.postValue(messages = new ArrayList<ChatMessage>());
+        }
+
+        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                if (viewType == 0) {
+                    SentMessageBinding sendBinding = SentMessageBinding.inflate(getLayoutInflater());
+                    return new MyRowHolder(sendBinding.getRoot());
+                } else {
+                    ReceiveMessageBinding receiveBinding = ReceiveMessageBinding.inflate(getLayoutInflater());
+                    return new MyRowHolder(receiveBinding.getRoot());
+                }
+
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+                holder.messageText.setText("");
+                holder.timeText.setText("");
+
+                ChatMessage obj = messages.get(position);
+                holder.messageText.setText(obj.getMessage());
+                holder.timeText.setText(obj.getTimeSent());
+            }
+
+            @Override
+            public int getItemCount() {
+                return messages.size();
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                if (messages.get(position).isSentButton() == true) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+
+            }
+        });
+
+        binding.sendButton.setOnClickListener(click -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd-MMM-yyy hh-mm-ss a");
+            String currentDateandTIme = sdf.format(new Date());
+            messages.add(new ChatMessage(binding.textInput.getText().toString(), currentDateandTIme, true));
+            myAdapter.notifyItemInserted(messages.size()-1);
+            binding.textInput.setText("");
+        });
+
+        binding.receiveButton.setOnClickListener(click -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd-MMM-yyy hh-mm-ss a");
+            String currentDateandTIme = sdf.format(new Date());
+            messages.add(new ChatMessage(binding.textInput.getText().toString(), currentDateandTIme, false));
+            myAdapter.notifyItemInserted(messages.size()-1);
+            binding.textInput.setText("");
+        });
+        binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
+
+
+    }
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timeText;
+
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+            timeText = itemView.findViewById(R.id.timeText);
+        }
+    }
+}
---
+++ app/src/main/java/algonquin/cst2335/anda0017/data/ChatMessage.java
@@ -0,0 +1,24 @@
+
+package algonquin.cst2335.anda0017.data;public class ChatMessage {
+    String message;
+    String timeSent;
+    boolean isSentButton;
+
+    public ChatMessage(String m, String t, boolean sent) {
+        message = m;
+        timeSent = t;
+        isSentButton = sent;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getTimeSent() {
+        return timeSent;
+    }
+
+    public boolean isSentButton() {
+        return isSentButton;
+    }
+}
